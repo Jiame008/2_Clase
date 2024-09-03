496022c0582806aeb80974d22b16c5631d4f8417
Jiame008 <jaimearmenterosv@gmail.com>
2024-09-02 22:18:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ longitud = int(input("Introduzca la longitud de la contraseña: "))
 contraseña = ""
 
 for i in range(longitud):
-    contraseña + random.choice(elementos)
+    contraseña += random.choice(elementos)
 
 print(contraseña)
