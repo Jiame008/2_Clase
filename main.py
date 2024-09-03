db496d55169480f14abb67a3ddc67455fa067be8
Jiame008 <jaimearmenterosv@gmail.com>
2024-09-02 22:14:54 -0400

    Create main.py
    Esta es la primera version del generador de contraseñas.

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+#Creando variable con todas los elementos posibles (72 valores)
+elementos = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+longitud = int(input("Introduzca la longitud de la contraseña: "))
+contraseña = ""
+
+for i in range(longitud):
+    contraseña + random.choice(elementos)
+
+print(contraseña)
